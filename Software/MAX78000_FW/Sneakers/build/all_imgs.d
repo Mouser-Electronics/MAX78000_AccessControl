ea170bf2ecd816759cbeda07764c28e28ce86419
mike parks <mbparks@gmail.com>
2021-05-06 00:42:42 -0400

    init commit

---
+++ Software/MAX78000_FW/Sneakers/build/all_imgs.d
@@ -0,0 +1,2 @@
+c:/Users/Home/eclipse-workspace/Sneakers/build/all_imgs.o: \
+ TFT/evkit/all_imgs.c
---
+++ Software/MAX78000_FW/Sneakers/build/startup_max78000.d
@@ -0,0 +1,2 @@
+c:/Users/Home/eclipse-workspace/Sneakers/build/startup_max78000.o: \
+ c:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
